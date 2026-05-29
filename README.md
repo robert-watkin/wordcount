b1de55477a657545e16f2ac291af42422fdab7ea
Robert Watkin <robert.watkin12@gmail.com>
2026-05-29 23:55:29 +0100

    wip

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # wcgo
+This is my first go cli project. This is intended to get me up to speed with Go fundamentals.
+The script will take in a file or read from stdin and count the number of words in it.
+
+## install
+
+go install wcgo
+
+## run
+
+wcgo README.md
+
+## example
+./wcgo README.md
--- main.go
+++ main.go
@@ -1,7 +1,85 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"cmp"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"slices"
+	"strings"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	// var iFlag = flag.String("i", "", "Input a file name")
+	flag.Parse()
+	var filename = flag.Arg(0)
+
+	// note:
+	// both os.Stdin and the return of os.Open are io.Reader
+	var src io.Reader
+
+	if flag.NArg() == 0 {
+		fmt.Println("No filename provided. Please provide input now:")
+
+		src = os.Stdin
+	} else {
+		// note:
+		// read a file and return []byte + error
+		// data, err := os.ReadFile(filename)
+		// if err = nil {
+		// 	log.Fatal(err)
+		// }
+
+		// open a file
+		data, err := os.Open(filename)
+		if err != nil {
+			log.Fatal(err)
+		}
+		src = data
+		defer data.Close() // data closed at the end of main
+	}
+
+	wordCountsMap := make(map[string]int)
+
+	// create a scanner to loop through each line
+	scanner := bufio.NewScanner(src)
+	for scanner.Scan() { // Scan() returns the string up to \n
+		words := strings.Split(scanner.Text(), " ")
+
+		for _, word := range words {
+			if word == "" {
+				continue
+			}
+			wordCountsMap[word]++
+			// fmt.Printf("Found %d %s\n", wordCounts[word], word)
+		}
+	}
+	fmt.Println("finished counting...")
+
+	// check for any errors that may have occurred
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "reading standard input:", err) // TODO what is diff between Fprintln and Println
+	}
+
+	// count results
+	countCmp = func(a, b int) int {
+		return cmp.Compare(a, b)
+	}
+
+	wordCountsSlice := make([]struct {
+		word  string
+		count int
+	}, len(wordCountsMap))
+	
+	for word, count := range wordCountsMap {
+		wordCountsSlice = append(wordCountsSlice, {word, count})
+	}
+
+	slices.Sort(wordCountsSlice)
+
+	fmt.Println("exiting...")
+	os.Exit(1)
 }
