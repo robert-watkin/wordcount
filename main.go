1c38d054835fd25a96bf4818f4bd0cbfb34b5b03
Robert Watkin <robert.watkin12@gmail.com>
2026-05-30 16:46:35 +0100

    feat: moved count functionality into it's own function

--- main.go
+++ main.go
@@ -29,12 +29,12 @@ func main() {
 
 	// note:
 	// both os.Stdin and the return of os.Open are io.Reader
-	var src io.Reader
+	var reader io.Reader
 
 	if flag.NArg() == 0 {
 		fmt.Println("No filename provided. Please provide input now:")
 
-		src = os.Stdin
+		reader = os.Stdin
 	} else {
 		// note:
 		// read a file and return []byte + error
@@ -48,33 +48,13 @@ func main() {
 		if err != nil {
 			log.Fatal(err)
 		}
-		src = data
+		reader = data
 		defer data.Close() // data closed at the end of main
 	}
 
-	wordCountsMap := make(map[string]int)
-
-	// create a scanner to loop through each line
-	scanner := bufio.NewScanner(src)
-	for scanner.Scan() { // Scan() returns the string up to \n
-		text := scanner.Text()
-		if !*caseSensitiveFlag {
-			text = strings.ToLower(text)
-		}
-
-		words := strings.Split(text, " ")
-
-		for _, word := range words {
-			if word == "" {
-				continue
-			}
-			wordCountsMap[word]++
-			// fmt.Printf("Found %d %s\n", wordCounts[word], word)
-		}
-	}
-
-	// check for any errors that may have occurred
-	if err := scanner.Err(); err != nil {
+	wordCountsMap, err := count(reader, caseSensitiveFlag)
+	// call to count with error handling
+	if err != nil {
 		fmt.Fprintln(os.Stderr, "reading standard input:", err) // TODO what is diff between Fprintln and Println
 	}
 
@@ -108,4 +88,28 @@ func main() {
 		fmt.Printf("%d %s\n", x.count, x.word)
 	}
 	os.Exit(1)
+}
+
+func count(r io.Reader, caseSensitiveFlag *bool) (map[string]int, error) {
+	wordCountsMap := make(map[string]int)
+
+	// create a scanner to loop through each line
+	scanner := bufio.NewScanner(r)
+	for scanner.Scan() { // Scan() returns the string up to \n
+		text := scanner.Text()
+		if !*caseSensitiveFlag {
+			text = strings.ToLower(text)
+		}
+
+		words := strings.Split(text, " ")
+
+		for _, word := range words {
+			if word == "" {
+				continue
+			}
+			wordCountsMap[word]++
+		}
+	}
+
+	return wordCountsMap, scanner.Err()
 }
